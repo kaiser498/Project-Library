ee44cb51961caa8a594bb41371d1535351e9028f
wayne <197704620+kaiser498@users.noreply.github.com>
2025-12-15 18:52:27 +0545

    Add an empty array for data storage

--- main.js
+++ main.js
@@ -6,3 +6,5 @@ const bookTitle = document.querySelector("#book_title");
 const authorName = document.querySelector("#author_name");
 const pageCount = document.querySelector("#page_count");
 const statusBtns = document.querySelectorAll('input[name="read_status"]');
+
+const myLibrary = [];
