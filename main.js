f731fa9f5a776a53b90bfd1949383cade6ae1aa2
wayne <197704620+kaiser498@users.noreply.github.com>
2025-12-18 17:44:21 +0545

    Select input for data entry to array

--- main.js
+++ main.js
@@ -13,3 +13,7 @@ const dialogForEntry = document.querySelector("#dialog-for-entry");
 addBtn.addEventListener("click", () => {
   dialogForEntry.showModal();
 });
+
+const bookTitle = document.querySelector("#book_title");
+const authorName = document.querySelector("#author_name");
+const pageCount = document.querySelector("#page_count");
