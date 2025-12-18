2724e1c10d49bd7cf070b878b4ad1513de3437f7
wayne <197704620+kaiser498@users.noreply.github.com>
2025-12-18 19:07:44 +0545

    Add S.N. cell
    Serial number is add according to table rows shown.

--- main.js
+++ main.js
@@ -32,8 +32,29 @@ function addNewBookToLibrary() {
 
 const tbody = document.querySelector("tbody");
 
-function updateTable(){
+function updateTable() {
   tbody.innerHTML = "";
 
   const fragment = document.createDocumentFragment();
+
+  for (const book of myLibrary) {
+    const newRow = document.createElement("tr");
+
+    const snCell = document.createElement("td");
+    const newTitleCell = document.createElement("td");
+    const newAuthorCell = document.createElement("td");
+    const newPageCell = document.createElement("td");
+    const statusCell = document.createElement("td");
+
+    snCell.textContent = tbody.rows.length + 1;
+    newTitleCell.textContent = book.title;
+    newAuthorCell.textContent = book.author;
+    newPageCell.textContent = book.pages;
+    statusCell.textContent = book.status;
+
+    newRow.append(newTitleCell, newAuthorCell, newPageCell);
+
+    fragment.append(newRow);
+  }
+  tbody.append(fragment);
 }
