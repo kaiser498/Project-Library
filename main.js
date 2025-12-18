9d7ff2e2b0bc5d7cc210499c29e6e56f65aeccf1
wayne <197704620+kaiser498@users.noreply.github.com>
2025-12-18 18:57:09 +0545

    Select "tbody" for adding rows of data

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const myLibrary = [];
 
-function Book(title, author, page) {
+function Book(title, author, page, status) {
   this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.page = page;
+  this.status = status;
 }
 
 const addBtn = document.querySelector("#addBtn");
@@ -17,4 +18,16 @@ addBtn.addEventListener("click", () => {
 const bookTitle = document.querySelector("#book_title");
 const authorName = document.querySelector("#author_name");
 const pageCount = document.querySelector("#page_count");
-const statusBtns = document.querySelectorAll('input[name="read_status"]');
+const statusBtns = document.querySelector('input[name="read_status"]:checked');
+
+function addNewBookToLibrary() {
+  let book = new Book(
+    bookTitle.value,
+    authorName.value,
+    pageCount.value,
+    statusBtns.value
+  );
+  myLibrary.push(book);
+}
+
+const tbody = document.querySelector("tbody");
